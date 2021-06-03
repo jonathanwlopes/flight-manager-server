097ac04e50719b04535010292eeeea28467bb918
jonathanwlopes <jonathanwlopes93@gmail.com>
2021-06-03 11:53:27 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+class People {
+  constructor(id, name, lastName){
+    this._id = id
+    this.name = name;
+    this.lastName = lastName
+  }
+}
+
+const user1 = new People(1,'Jonathan','Lopes')
+
+const dbApp = {
+  userList: [user1],
+
+  getUsers: () => {
+    const userList = dbApp.userList;
+
+    return [...userList];
+  },
+
+  getUserById: (userId) => {
+    const userList = dbApp.getUsers();
+
+    const user = userList.find((user) => user._id === userId);
+
+    return user;
+  },
+};
+
+app.get("/", (req, res) => {
+  res.send("PONG");
+});
+
+app.get("/users", (req, res) => {
+  return res.send([...dbApp.userList]);
+});
+
+app.listen(port, () => {
+  console.log("Ouvindo servidor");
+});
